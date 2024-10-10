007213d186d4782c44fdf3317cea02bdaae36060
Banjo Adetokunbo <banjotokunboh@gmail.com>
2024-10-10 15:47:57 +0100

    For fetching my movie Api

---
+++ movie-database/src/services/FetchData.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+const API_KEY = "1ba32adb";
+const BASE_URL = 'http://www.omdbapi.com/';
+
+//Function to fetch movies using titles
+export const fetchMoviesByTitle = async (titles) => {
+    const movieRequests = titles.map((title) =>
+        axios.get(`${BASE_URL}?apikey=${API_KEY}&t=${title}`)
+    );
+
+    try{
+        const movieResponses = await Promise.all(movieRequests);
+        return movieResponses.map(response => response.data);
+    } catch (error) {
+        console.error('Error fetching movies:', error);
+        return [];
+    }
+};
+
+
+
+
+  
+
+
+
+
+
